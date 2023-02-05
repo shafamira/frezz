6913f55ed31a661e327c2681fbd53be7aaf37670
Shafa Amira <shafaqonitatin@gmail.com>
2023-02-05 22:54:29 +0700

    update model

--- src/pages/Classify.js
+++ src/pages/Classify.js
@@ -13,7 +13,7 @@ import './Classify.css';
 import 'cropperjs/dist/cropper.css';
 
 
-const MODEL_PATH = '/model/model3/transfer_learning/model.json';
+const MODEL_PATH = '/rotten-fresh-model.h5';
 const IMAGE_SIZE = 150;
 const CANVAS_SIZE = 224;
 const TOPK_PREDICTIONS = 3;
